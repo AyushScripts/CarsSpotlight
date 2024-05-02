e5620e4e66587b987f720d220c7893a39cc45ffc
AyushScripts <ayushtheking117@gmail.com>
2024-05-02 21:42:59 +0530

    Updates

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,60 +1,81 @@
 "use client"
 
-import { SearchManufacturerProps } from '@/types'
-import { Combobox, Transition } from '@headlessui/react'
-import Image from 'next/image'
-import { useState, Fragment } from 'react'
-import { manufacturers } from '@/constants'
+import React from "react";
+import Image from "next/image";
+import { Fragment, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import { IoIosArrowDown } from "react-icons/io";
 
-const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
-  const [query, setQuery] = useState('');
+import { manufacturers } from "@/constants";
+import { SearchManufacturerProps } from "@/types";
 
-  const filteredManufacturers = query === '' ? manufacturers : manufacturers.filter((item)=>(
-    item.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, ""))
-  ))
-  
+const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
   return (
     <div className='search-manufacturer'>
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className='relative w-full'>
-          <Combobox.Button className="absolute top-[14px]">
-            <Image 
-              src="/car-logo.svg"
-              alt="Logo"
+          {/* Button for the combobox. Click on the icon to see the complete dropdown */}
+          <Combobox.Button className='absolute top-[14px]'>
+            <Image
+              src='/car-logo.svg'
               width={20}
               height={20}
-              className="ml-4"
+              className='ml-4'
+              alt='car logo'
             />
           </Combobox.Button>
 
+          {/* Input field for searching */}
           <Combobox.Input
-            className="search-manufacturer__input"
-            placeholder="Tesla"
-            displayValue={(manufacturer: string)=> manufacturer}
-            onChange={(e)=>setQuery(e.target.value)}
+            className='search-manufacturer__input'
+            displayValue={(item: string) => item}
+            onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
+            placeholder='Volkswagen...'
           />
 
+          {/* Transition for displaying the options */}
           <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            afterLeave={()=> setQuery('')}
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => setQuery("")} // Reset the search query after the transition completes
+          >
+            <Combobox.Options
+              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+              static
             >
-              <Combobox.Options>
-                {filteredManufacturers.length === 0 && query !=="" ? (
-                  <div className='w-full'>No brand found.</div>
-                ) : (
-                  filteredManufacturers.map((item) =>(
-                    <Combobox.Option
-                      key={item}
-                      className={({active})=> `
-                      relative search-manufacturer__option
-                      ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}
-                      `}
-                      value={item}
-                    >
-                      {({ selected, active }) => (
+              {filteredManufacturers.length === 0 && query !== "" ? (
+                <Combobox.Option
+                  value={query}
+                  className='search-manufacturer__option'
+                >
+                  Create "{query}"
+                </Combobox.Option>
+              ) : (
+                filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? "bg-primary-blue text-white" : "text-gray-900"
+                      }`
+                    }
+                    value={item}
+                  >
+                    {({ selected, active }) => (
                       <>
                         <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
                           {item}
@@ -67,18 +88,15 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
                         ) : null}
                       </>
                     )}
-                    </Combobox.Option>
-                  ))
-                )
-                }
-              </Combobox.Options>
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
           </Transition>
-            
-          
         </div>
       </Combobox>
     </div>
-  )
-}
+  );
+};
 
-export default SearchManufacturer
+export default SearchManufacturer;
---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2d65d6dc0fmshccfa6198e6226bfp14eb32jsn1017eefb902f',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
